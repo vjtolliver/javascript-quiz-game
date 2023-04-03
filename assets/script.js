edd7d6d1f0b1bdecc5fe1fc8ee561df5687a9040
vjtolliver <vj.tolliver@gmail.com>
2023-04-02 17:12:55 -0700

    added questions array of objects

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,43 @@
-var questionsArr = {
-    [Question 1: "What is question one?",
-]
+var questionsArr = [
+    {Question:"1. What is question one?",
+    answers: ["A. This is an answer option", "B. This is an answer option", "C. This is an answer option", "D. This is an answer option"],
+    isCorrect: "D. This is an answer option"
+},
+
+    {Question:"2. What is question two?",
+    answers: ["A. This is an answer option", "B. This is an answer option", "C. This is an answer option", "D. This is an answer option"],
+    isCorrect: "B. This is an answer option"
+},
+    {Question:"3. What is question three?",
+    answers: ["A. This is an answer option", "B. This is an answer option", "C. This is an answer option", "D. This is an answer option"],
+    isCorrect: "A. This is an answer option"
+},
+    {Question:"4. What is question four?",
+    answers: ["A. This is an answer option", "B. This is an answer option", "C. This is an answer option", "D. This is an answer option"],
+    isCorrect:"D. This is an answer option"
+},
+    {Question: "5. What is question five?",
+    answers: ["A. This is an answer option", "B. This is an answer option", "C. This is an answer option", "D. This is an answer option"],
+    isCorrect: "C. This is an answer option"
+},
+    {Question:"6. What is question six?",
+    answers: ["A. This is an answer option", "B. This is an answer option", "C. This is an answer option", "D. This is an answer option"],
+    isCorrect: "C. This is an answer option"
+},
+    {Question:"7. What is question seven?",
+    answers: ["A. This is an answer option", "B. This is an answer option", "C. This is an answer option", "D. This is an answer option"],
+    isCorrect: "A. This is an answer option"
+},
+    {Question:"8. What is question eight?",
+    answers: ["A. This is an answer option", "B. This is an answer option", "C. This is an answer option", "D. This is an answer option"],
+    isCorrect: "D. This is an answer option"
+},
+    {Question:"9. What is question nine?",
+    answers: ["A. This is an answer option", "B. This is an answer option", "C. This is an answer option", "D. This is an answer option"],
+    isCorrect: "B. This is an answer option"
+},
+    {Question:"10. What is question ten?",
+    answers: ["A. This is an answer option", "B. This is an answer option", "C. This is an answer option", "D. This is an answer option"],
+    isCorrect: "A. This is an answer option"
 }
+]
